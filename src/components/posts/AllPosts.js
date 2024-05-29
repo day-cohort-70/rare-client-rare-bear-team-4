861b16e9892b5e4b2173bd4b130e3bb9402975ea
Nicholas Davison <nick.a.davison@gmail.com>
2024-05-29 16:01:23 -0400

    allposts workign as designed

--- src/components/posts/AllPosts.js
+++ src/components/posts/AllPosts.js
@@ -1,10 +1,19 @@
 import "./posts.css"
 export const AllPosts = ({allPosts, getAndSetAllPosts}) => {
 
+    const sortedPosts = allPosts.sort((a, b) => {
+        const dateA = new Date(a.publicationDate);
+        const dateB = new Date(b.publicationDate);
+        return dateB - dateA;
+    })
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+
     return (
         <div className="page-container">
             {allPosts.map((post) => {
-                return post.approved ? (
+                const publicationDate = new Date(post.publicationDate)
+                return post.approved && publicationDate < today ? (
                     <div key={post.id} className="post-item">
                         <h2>{post.title}</h2>
                         <h2>{post.user.userFirstName} {post.user.userLastName}</h2>
