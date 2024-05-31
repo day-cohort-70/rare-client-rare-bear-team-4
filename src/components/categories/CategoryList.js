9aed0cc9fdad0fb3335ecb1b558e8aa9c4f76416
Sydney Sharp <sydney.marie.sharp@gmail.com>
2024-05-31 10:33:26 -0500

    added a space to create a new category

---
+++ src/components/categories/CategoryList.js
@@ -0,0 +1,54 @@
+// CategoryList.js
+import React, { useEffect, useState } from 'react';
+import { getAllCategories, saveCategoriesToDatabase } from '../../managers/CategoryManager';
+import './CategoryList.css';
+
+export const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+  const [newLabel, setNewLabel] = useState("")
+  useEffect(() => {
+    getAllCategories().then(catObjs => {
+      const sortedCategories = catObjs.sort((a, b) => a.label.localeCompare(b.label));
+      setCategories(sortedCategories);
+    });
+  }, []);
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const newPost = {
+        label: newLabel
+    };
+    const savedCategory = await saveCategoriesToDatabase(newPost);
+    if (savedCategory) {
+        setNewLabel("");
+        // Fetch the updated list of tags and update the state
+        const updatedCategories = await getAllCategories();
+        setCategories(updatedCategories);
+    }
+
+}
+
+  return (
+    <div className='full-page'>
+      <div className="category-list">
+        {categories.map((category) => (
+          <div key={category.id} className="category-card">
+            <h2>{category.label}</h2>
+          </div>
+        ))}
+      </div>
+      <section className="leftSection">
+                <form className="create-post-form" onSubmit={handleSave}>
+                    <h2 className="card-title">Create New Tag</h2>
+                    <div className="form-group">
+                        <label></label>
+                        <input type="text" value={newLabel} onChange={(event) => setNewLabel(event.target.value)} required />
+                    </div>
+                    <div className="form-group">
+                    <button type="submit" className="btn-save">Create</button>
+                </div>
+                </form>
+            </section>
+    </div>
+  );
+};
--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -1,3 +1,13 @@
 export const getAllCategories = () => {
     return fetch("http://localhost:8088/categories").then((res) => res.json())
 } 
+
+export const saveCategoriesToDatabase = (post) => {
+    return fetch(`http://localhost:8088/categories`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(post)
+    }).then((res) => res.json())
+}
