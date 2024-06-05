3a138f4ad297aabda7e792ac47918cb0019fca5e
Sydney Sharp <sydney.marie.sharp@gmail.com>
2024-06-05 16:30:03 -0500

    added ability to edit a tag

--- src/managers/TagService.js
+++ src/managers/TagService.js
@@ -1,6 +1,9 @@
 export const getAllTags = () => {
     return fetch("http://localhost:8088/tags").then((res) => res.json())
 }
+export const fetchTagByID = (tagId, tagDate) => {
+    return fetch(`http://localhost:8088/tags/${tagId}`).then((res) => res.json())
+}
 
 export const saveTagToDatabase = (post) => {
     return fetch(`http://localhost:8088/tags`, {
@@ -25,3 +28,16 @@ export const deleteTag = (tagId) => {
         console.error('There has been a problem with your fetch operation:', error);
     });
 };
+
+
+
+
+    export const updateTag = (tagId, tag_data) => {
+        return fetch(`http://localhost:8088/tags/${tagId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(tag_data)
+        })
+    }
